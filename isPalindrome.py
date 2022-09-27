2372cdb7537b4651bd49c8c745cd47067fd44567
gsingh151 <45342803+gsingh151@users.noreply.github.com>
2022-09-26 23:12:07 -0400

    fix

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,24 +1,19 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        left = 0
-        right = len(s) - 1
-        while (left < right):
-            while (left < right) and not self.alphanum(s[left]):
-                left += 1 
-            while (right > left) and not self.alphanum(s[right]):
-                right -= 1
-            if s[left].lower() != s[right].lower():
-                return False
-            left += 1
+def isPalindrome(self, s):
+    left = 0
+    right = len(s) - 1
+    while (left < right):
+        while (left < right) and not self.alphanum(s[left]):
+            left += 1 
+        while (right > left) and not self.alphanum(s[right]):
             right -= 1
-        return True
-    
-    def alphanum(self, c):
-        return (ord('a') <= ord(c) <= ord('z') or
-                ord('A') <= ord(c) <= ord('Z') or
-                ord('0') <= ord(c) <= ord('9'))
+        if s[left].lower() != s[right].lower():
+            return False
+        left += 1
+        right -= 1
+    return True
+
+def alphanum(self, c):
+    return (ord('a') <= ord(c) <= ord('z') or
+            ord('A') <= ord(c) <= ord('Z') or
+            ord('0') <= ord(c) <= ord('9'))
         
